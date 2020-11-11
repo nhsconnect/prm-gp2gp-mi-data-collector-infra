3a63e1a97c73e283b466a7645bd4fe2ac3695146
James Green <jagreen@thoughtworks.com>
2020-11-11 10:22:04 +0000

    PRMT-1262 [JG/DS] Introduce S3 default encryption on MI data bucket

--- terraform/s3-v2.tf
+++ terraform/s3-v2.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "mi_data_v2" {
   bucket = "prm-gp2gp-mi-data-${var.environment}-v2"
   acl    = "private"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = merge(
     local.common_tags,
     {
