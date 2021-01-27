72e88275050ae9f2d9d9c2e9fa00401fe6d15282
Helen Zhou <helen.zhou1@nhs.net>
2021-01-27 15:33:22 +0000

    PRMT-1479 Add Cloudwatch Dashboard with forward message metric widget

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -4,9 +4,35 @@ resource "aws_cloudwatch_log_metric_filter" "forward_message_event" {
   log_group_name = aws_cloudwatch_log_group.mesh_s3_forwarder.name
 
   metric_transformation {
-    name      = "ForwardMessageEventCount"
-    namespace = "MeshS3Forwarder/${var.environment}"
-    value     = 1
+    name          = "ForwardMessageEventCount"
+    namespace     = "MeshS3Forwarder/${var.environment}"
+    value         = 1
     default_value = 0
   }
+}
+
+resource "aws_cloudwatch_dashboard" "mesh_s3_forwarder" {
+  dashboard_name = "${var.environment}-mesh-s3-forwarder"
+  dashboard_body = jsonencode({
+    "widgets" : [
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 0,
+        "width" : 6,
+        "height" : 3,
+        "properties" : {
+          "metrics" : [
+            [
+              "${aws_cloudwatch_log_metric_filter.forward_message_event.metric_transformation[0].namespace}",
+              "${aws_cloudwatch_log_metric_filter.forward_message_event.name}"
+            ]
+          ],
+          "period" : 300,
+          "region" : var.region,
+          "title" : "${aws_cloudwatch_log_metric_filter.forward_message_event.name}"
+        }
+      }
+    ]
+  })
 }
