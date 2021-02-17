c3a56bdeac7b32ec602c9fedd7492253db934f58
matthew-peers <matthew.peers2@nhs.net>
2021-02-17 16:35:54 +0000

    PRMT-1479 [MP] Add filled service count metric to monitoring dashboard

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -201,12 +201,13 @@ resource "aws_cloudwatch_dashboard" "mi_data_collector" {
         "height" : 6,
         "properties" : {
           "metrics" : [
+            [{ "expression" : "FILL(METRICS(), 0)", "label" : "Fill", "id" : "e1" }],
             [
               "ECS/ContainerInsights",
               "ServiceCount",
               "ClusterName",
               aws_ecs_cluster.mi_data_collector.name,
-              { "stat" : "Average" }
+              { "stat" : "Average", "visible" : false }
             ]
           ],
           "region" : var.region,
