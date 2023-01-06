ba89a33d83383a229948795134fb2c2d46152c2c
Jake Newman <jacob.newman1@nhs.net>
2023-01-06 15:29:43 +0000

    PRMT-3027 [JN/JP] Fix tags to make them all strings

--- terraform/tags.tf
+++ terraform/tags.tf
@@ -4,14 +4,14 @@ locals {
     Environment         = var.environment
     envtype             = var.environment
     Team                = var.team
-    TagVersion          = 1
+    TagVersion          = "1"
     Programme           = "GP IT Futures"
     Project             = "Patient Record Migration"
     Owner               = "Candice Moore"
     CostCentre          = "P0688/04"
     Customer            = "NHS D"
-    data_classification = 1
-    DataType            = None
+    data_classification = "1"
+    DataType            = "None"
     ProjectType         = "Public beta"
     PublicFacing        = "Y"
     ServiceCategory     = "Bronze"
